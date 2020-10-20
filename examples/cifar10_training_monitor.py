4af12bd9273dca065fcb937bc13018a2a0ea00e7
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-10-21 04:54:43 +0530

    json path bug

--- examples/cifar10_training_monitor.py
+++ examples/cifar10_training_monitor.py
@@ -11,54 +11,62 @@ from sklearn.preprocessing import LabelBinarizer
 from DL4CV.NeuralNetworks.miniVGGnet import MiniVGGNet
 from DL4CV.CallBacks.trainingMonitor import TrainingMonitor
 
-# construct the argument parser
-ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", required=True,
-                help="Path to the output Directory")
-args = vars(ap.parse_args())
 
-# show information on the Process ID
-print(f"[INFO] Process ID: {os.getpid()}")
+def main():
+    # construct the argument parser
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-o", "--output", default="images/Training Monitor/",
+                    help="Path to the output Directory")
+    args = vars(ap.parse_args())
 
-# load the training data and testing data
-# scale them into range(0, 1)
-print("[INFO] Loading the CIFAR-10 data...")
-(X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
-X_train = X_train.astype("float") / 255.0
-X_test = X_test.astype("float") / 255.0
+    # show information on the Process ID
+    print(f"[INFO] Process ID: {os.getpid()}")
 
-# conert the labels from integers to vectors
-lb = LabelBinarizer()
-Y_train = lb.fit_transform(Y_train)
-Y_test = lb.fit_transform(Y_test)
+    # load the training data and testing data
+    # scale them into range(0, 1)
+    print("[INFO] Loading the CIFAR-10 data...")
+    (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+    X_train = X_train.astype("float") / 255.0
+    X_test = X_test.astype("float") / 255.0
 
-# initialize the label names of the cifar-10 dataset
-labelNames = ["airplane",
-              "automobile",
-              "bird",
-              "cat",
-              "deer",
-              "dog",
-              "frog",
-              "horse",
-              "ship",
-              "truck"]
+    # conert the labels from integers to vectors
+    lb = LabelBinarizer()
+    Y_train = lb.fit_transform(Y_train)
+    Y_test = lb.fit_transform(Y_test)
 
-# initialize the SGD optimizer but without any learning rate decay
-print("[INFO] Compiling the model...")
-opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
-model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
-model.compile(loss="categorical_crossentropy",
-              optimizer=opt,
-              metrics=["accuracy"])
+    # initialize the label names of the cifar-10 dataset
+    labelNames = ["airplane",
+                  "automobile",
+                  "bird",
+                  "cat",
+                  "deer",
+                  "dog",
+                  "frog",
+                  "horse",
+                  "ship",
+                  "truck"]
 
-# construct the set of callbacks
-figPath = os.path.sep.join([args["output"], f"{os.getpid()}.png"])
-jsonPath = os.path.sep.join([args["output"], f"{os.getpid()}.json"])
+    # initialize the SGD optimizer but without any learning rate decay
+    print("[INFO] Compiling the model...")
+    opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
+    model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=opt,
+                  metrics=["accuracy"])
 
-callbacks = [TrainingMonitor(figPath=figPath, jsonPath=jsonPath)]
+    # construct the set of callbacks
+    figPath = os.path.sep.join([args["output"], f"{os.getpid()}.png"])
+    jsonPath = os.path.sep.join([args["output"], f"{os.getpid()}.json"])
 
-print("[INFO] Training the network...")
-model.fit(X_train, Y_train,
-          validation_data=(X_test, Y_test),
-          batch_size=64, epochs=100, callbacks=callbacks, verbose=1)
+    callbacks = [TrainingMonitor(figPath=figPath, jsonPath=jsonPath)]
+
+    print("[INFO] Training the network...")
+    model.fit(X_train, Y_train,
+              validation_data=(X_test, Y_test),
+              batch_size=64, epochs=100, callbacks=callbacks, verbose=1)
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
